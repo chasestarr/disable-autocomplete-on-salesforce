2fbd8083b378fcdb1cf9b53ce8c0a9199fd88c99
Chase Starr <chasestarr@gmail.com>
2018-06-26 09:51:57 -0700

    Update disable-salesforce-autocomplete.js

--- disable-salesforce-autocomplete.js
+++ disable-salesforce-autocomplete.js
@@ -3,5 +3,5 @@ console.log('disabling autocomplete on this page');
 var inputElements = document.getElementsByTagName('input');
 
 for (i = 0; inputElements[i]; i++) {
-  inputElements[i].setAttribute('autocomplete', 'off');
+  inputElements[i].setAttribute('autocomplete', 'false');
 }
